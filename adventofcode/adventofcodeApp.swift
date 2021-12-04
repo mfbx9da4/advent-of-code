4cb33d48f7b2670fe2ef199396274c35a10c28d0
David Alberto Adler <dalberto.adler@gmail.com>
2021-12-04 11:13:00 +0000

    Initial Commit

---
+++ adventofcode/adventofcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  adventofcodeApp.swift
+//  adventofcode
+//
+//  Created by David Adler on 04/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct adventofcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
